e0ab60677b20d6511ebf71b63162818fc377f1d3
Seyit Behrullah Ateş <117732354+behrullahats@users.noreply.github.com>
2023-07-19 11:17:42 +0300

    Create 4.Class_Exists.php

---
+++ 4.Class_Exists.php
@@ -0,0 +1,17 @@
+<?php
+
+//Class_Exists() Bir fonksiyonun tanımlı olup olmadığını kontrol eder .
+
+class Sinif
+{
+    public function ders()
+    {
+        echo "Ders Methodu";
+    }
+}
+
+if (class_exists("Sinif")) {
+    echo "Sinif Adı altında vardır";
+} else {
+    echo "Yoktur";
+}
